0109601fad408ec67585b87ae63cb8cad776fca3
adisco4420 <adisco4420@gmail.com>
2017-10-22 14:53:30 +0100

    component part1

---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,57 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Dish } from '../shared/dish';
+
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss']
+})
+export class MenuComponent implements OnInit {
+
+  dishes:Dish[] = [
+                    {
+                      name:'uthappizza',
+                      image:'/assets/images/uthappizza.png',
+                      catergory:'mains',
+                      label:'Hot',
+                      price:'4.99',
+                      description:'A unique combination of sweet food'
+
+                    },
+                    {
+                      name:'Zucchipakoda',
+                      image:'/assets/images/zucchipakoda.png',
+                      catergory:'appetizer',
+                      label:'',
+                      price:'1.99',
+                      description:'Deep friend Zucchapozada'
+
+                    },
+                    {
+                      name:'Vadonut',
+                      image:'/assets/images/vadonut.png',
+                      catergory:'appetizer',
+                      label:'New',
+                      price:'1.99',
+                      description:'A quintessential ConfFusion of the food is delicious'
+
+                    },
+                    {
+                      name:'ElaiCheese Cake',
+                      image:'/assets/images/elaicheesecake.png',
+                      catergory:'desert',
+                      label:'',
+                      price:'2.99',
+                      description:'A delecttable semi-sweet food taste very delicious'
+
+                    }
+                  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/dish.ts
@@ -0,0 +1,8 @@
+export class Dish {
+    name:string;
+    image:string;
+    catergory:string;
+    label:string;
+    price:string;
+    description:string;
+}
