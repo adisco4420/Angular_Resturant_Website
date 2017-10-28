8c4f40cbe50a66523cc4212e54b67767a3407ace
adisco4420 <adisco4420@gmail.com>
2017-10-28 20:50:53 +0100

    HTTP part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -27,6 +27,7 @@ export class DishdetailComponent implements OnInit {
   com:any;
   value:any;
   comments:any;
+  errMess:string
 
 
   array:any;
@@ -67,7 +68,8 @@ export class DishdetailComponent implements OnInit {
               this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
               this.route.params
                 .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-                .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+                .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id),
+                  errmess => this.errMess = <any>errmess; });
             }
             setPrevNext(dishId: number) {
               let index = this.dishIds.indexOf(dishId);
