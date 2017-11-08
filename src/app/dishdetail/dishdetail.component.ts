b655897e9dfcd2ead5a00cd98ce8961ffbf95f8b
adisco4420 <adisco4420@gmail.com>
2017-11-08 15:34:47 +0100

    Animation Part 1

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -4,18 +4,32 @@ import {Dish} from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 import { FormBuilder,FormGroup,Validators } from '@angular/forms';
-import { dishFeedback } from '../shared/feedback';
+
 import { Comment } from '../shared/comment';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { trigger, state , style, animate, transition} from '@angular/animations'
 
 import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
-  styleUrls: ['./dishdetail.component.scss']
+  styleUrls: ['./dishdetail.component.scss'],
+  animations:[
+    trigger('visibility',[
+      state('shown',style({
+        transform:'scale(1.0)',
+        opacity:1
+      })),
+      state('hidden', style({
+        transform:'scale(0.5)',
+        opacity:0
+      })),
+      transition('* => *', animate('5s ease-in-out'))
+    ])
+  ]
 })
 export class DishdetailComponent implements OnInit {
  
@@ -24,7 +38,8 @@ export class DishdetailComponent implements OnInit {
   dishcopy = null;
   prev:number;
   next:number;
-  errMess:string
+  errMess:string;
+  visibility= 'shown';
 
   feedbackForm:FormGroup;
   comment:Comment;
@@ -62,9 +77,9 @@ export class DishdetailComponent implements OnInit {
           this.createForm();
               this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
               this.route.params
-                .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
-                .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id),
-                  errmess => this.errMess = <any>errmess; });
+                .switchMap((params: Params) => {this.visibility= 'hidden';return  this.dishservice.getDish(+params['id']); })
+                .subscribe(dish => { this.dish = dish; this.dishcopy = dish; this.setPrevNext(dish.id); this.visibility= 'shown'},
+                  errmess => {this.dish= null; this.errMess = <any>errmess });
             }
             setPrevNext(dishId: number) {
               let index = this.dishIds.indexOf(dishId);
